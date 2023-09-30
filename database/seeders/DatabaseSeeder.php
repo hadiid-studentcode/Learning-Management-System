44b157f54e1beb21bb09fd3627337d15b1e86c80
hadiid-studentcode <hadiidandri2000@gmail.com>
2023-09-30 23:10:08 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,34 +14,34 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        \App\Models\TahunAjaran::factory()->create([
-            'tahun_ajaran' => '2023-2024',
+        // \App\Models\TahunAjaran::factory()->create([
+        //     'tahun_ajaran' => '2023-2024',
 
-        ]);
+        // ]);
 
         \App\Models\User::factory()->create([           // 16
             'nama_lengkap' => 'admin',
-            'email' => 'admin@gmail.com',
+            'email' => null,
             'userid' => 'admin',
             'password' => bcrypt('admin'),
-            'foto' => fake()->imageUrl('200', '200', 'people'),
+            'foto' => null,
             'hak_akses' => 'Super User',
             'remember_token' => Str::random(10),
         ]);
 
         \App\Models\User::factory()->create([       // 15
-            'nama_lengkap' => 'Fikri',
+            'nama_lengkap' => 'Tata Usaha',
             'email' => fake()->unique()->safeEmail(),
             'userid' => 'fikri',
-            'password' => bcrypt('fikri1208201'),
-            'foto' => fake()->imageUrl('200', '200', 'people'),
+            'password' => bcrypt('fikri'),
+            'foto' => null,
             'hak_akses' => 'Tata Usaha',
             'remember_token' => Str::random(10),
         ]);
 
         \App\Models\Pegawai::factory()->create([
             'nik' => fake()->numerify('##########'),
-            'nama' => 'Fikri',
+            'nama' => 'Tata Usaha',
             'jenis' => 'Ketua Tata Usaha',
             'no_hp' => fake()->phoneNumber(),
             'tempat_lahir' => fake()->city(),
@@ -58,102 +58,102 @@ class DatabaseSeeder extends Seeder
             'id_user' => 2, //15
         ]);
 
-        \App\Models\User::factory()->create([       // 13
-            'nama_lengkap' => 'Hadiid Andri Yulison',
-            'email' => fake()->unique()->safeEmail(),
-            'userid' => '12345',
-            'password' => bcrypt('12345'),
-            'foto' => fake()->imageUrl('200', '200', 'people'),
-            'hak_akses' => 'Guru',
-            'remember_token' => Str::random(10),
-        ]);
+        // \App\Models\User::factory()->create([       // 13
+        //     'nama_lengkap' => 'Hadiid Andri Yulison',
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'userid' => '12345',
+        //     'password' => bcrypt('12345'),
+        //     'foto' => fake()->imageUrl('200', '200', 'people'),
+        //     'hak_akses' => 'Guru',
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        \App\Models\Guru::factory()->create([
-            'nik' => fake()->numerify('##########'),
-            'nama' => 'Hadiid Andri Yulison',
-            'nig' => '12345',
-            'status' => 'Tetap',
-            'jenis' => 'Wali Kelas',
-            'studi_keahlihan' => 'fisika',
-            'jenis_kelamin' => 'laki-laki',
-            'nohp' => fake()->phoneNumber(),
-            'tempat_lahir' => fake()->city(),
-            'tanggal_lahir' => fake()->date(),
-            'agama' => 'islam',
-            'kelurahan' => fake()->city(),
-            'kecamatan' => fake()->city(),
-            'kabupatenKota' => fake()->city(),
-            'provinsi' => fake()->city(),
-            'alamat' => fake()->address(),
-            'foto' => '1686726289999-arief.jpg',
-            'id_user' => 3,
+        // \App\Models\Guru::factory()->create([
+        //     'nik' => fake()->numerify('##########'),
+        //     'nama' => 'Hadiid Andri Yulison',
+        //     'nig' => '12345',
+        //     'status' => 'Tetap',
+        //     'jenis' => 'Wali Kelas',
+        //     'studi_keahlihan' => 'fisika',
+        //     'jenis_kelamin' => 'laki-laki',
+        //     'nohp' => fake()->phoneNumber(),
+        //     'tempat_lahir' => fake()->city(),
+        //     'tanggal_lahir' => fake()->date(),
+        //     'agama' => 'islam',
+        //     'kelurahan' => fake()->city(),
+        //     'kecamatan' => fake()->city(),
+        //     'kabupatenKota' => fake()->city(),
+        //     'provinsi' => fake()->city(),
+        //     'alamat' => fake()->address(),
+        //     'foto' => '1686726289999-arief.jpg',
+        //     'id_user' => 3,
 
-        ]);
+        // ]);
 
-        \App\Models\Kelas::factory()->create([
-            'nama' => '4',
-            'rombel' => 'ibrahim',
-            'id_guru' => 1,
-            'kouta_siswa' => 30,
-            'id_tahun_ajaran' => 1,
+        // \App\Models\Kelas::factory()->create([
+        //     'nama' => '4',
+        //     'rombel' => 'ibrahim',
+        //     'id_guru' => 1,
+        //     'kouta_siswa' => 30,
+        //     'id_tahun_ajaran' => 1,
 
-        ]);
+        // ]);
 
-        \App\Models\User::factory()->create([           // 14
-            'nama_lengkap' => 'Andriy Hadiid',
-            'email' => fake()->unique()->safeEmail(),
-            'userid' => '121212',
-            'password' => bcrypt('121212'),
-            'foto' => fake()->imageUrl('200', '200', 'people'),
-            'hak_akses' => 'Siswa',
-            'remember_token' => Str::random(10),
-        ]);
+        // \App\Models\User::factory()->create([           // 14
+        //     'nama_lengkap' => 'Andriy Hadiid',
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'userid' => '121212',
+        //     'password' => bcrypt('121212'),
+        //     'foto' => fake()->imageUrl('200', '200', 'people'),
+        //     'hak_akses' => 'Siswa',
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        \App\Models\Siswa::factory()->create([  // 11
-            'nisn' => '121212',
-            'nama' => 'Andriy Hadiid',
-            'id_kelas' => 1,
-            'jenis_kelamin' => 'laki-laki',
-            'agama' => 'Islam',
-            'kelurahan' => fake()->city(),
-            'kecamatan' => fake()->city(),
-            'kabupatenKota' => fake()->city(),
-            'provinsi' => fake()->city(),
-            'alamat' => fake()->address(),
-            'tempat_lahir' => fake()->city(),
-            'tanggal_lahir' => fake()->date(),
-            'foto' => '1686726289999-arief.jpg',
-            'id_user' => 4,
+        // \App\Models\Siswa::factory()->create([  // 11
+        //     'nisn' => '121212',
+        //     'nama' => 'Andriy Hadiid',
+        //     'id_kelas' => 1,
+        //     'jenis_kelamin' => 'laki-laki',
+        //     'agama' => 'Islam',
+        //     'kelurahan' => fake()->city(),
+        //     'kecamatan' => fake()->city(),
+        //     'kabupatenKota' => fake()->city(),
+        //     'provinsi' => fake()->city(),
+        //     'alamat' => fake()->address(),
+        //     'tempat_lahir' => fake()->city(),
+        //     'tanggal_lahir' => fake()->date(),
+        //     'foto' => '1686726289999-arief.jpg',
+        //     'id_user' => 4,
 
-        ]);
+        // ]);
 
-        \App\Models\User::factory()->create([       // 12
-            'nama_lengkap' => 'Wahyu Ardian',
-            'email' => fake()->unique()->safeEmail(),
-            'userid' => 'wahyu ardian',
-            'password' => bcrypt('wahyu ardian'),
-            'foto' => fake()->imageUrl('200', '200', 'people'),
-            'hak_akses' => 'Wali Murid',
-            'remember_token' => Str::random(10),
-        ]);
+        // \App\Models\User::factory()->create([       // 12
+        //     'nama_lengkap' => 'Wahyu Ardian',
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'userid' => 'wahyu ardian',
+        //     'password' => bcrypt('wahyu ardian'),
+        //     'foto' => fake()->imageUrl('200', '200', 'people'),
+        //     'hak_akses' => 'Wali Murid',
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        \App\Models\WaliMurid::factory()->create([
-            'nik' => fake()->numerify('##########'),
-            'nama' => 'Wahyu Ardian',
-            'hubungan' => 'Saudara',
-            'jenis_kelamin' => 'Laki-laki',
-            'agama' => 'Islam',
-            'no_hp' => fake()->phoneNumber(),
-            'kelurahan' => fake()->streetName(),
-            'kecamatan' => fake()->streetName(),
-            'kabupatenKota' => fake()->city(),
-            'provinsi' => fake()->city(),
-            'email' => null,
-            'pekerjaan' => fake()->jobTitle(),
-            'alamat' => fake()->address(),
-            'id_siswa' => 1,
-            'id_user' => 5,
-        ]);
+        // \App\Models\WaliMurid::factory()->create([
+        //     'nik' => fake()->numerify('##########'),
+        //     'nama' => 'Wahyu Ardian',
+        //     'hubungan' => 'Saudara',
+        //     'jenis_kelamin' => 'Laki-laki',
+        //     'agama' => 'Islam',
+        //     'no_hp' => fake()->phoneNumber(),
+        //     'kelurahan' => fake()->streetName(),
+        //     'kecamatan' => fake()->streetName(),
+        //     'kabupatenKota' => fake()->city(),
+        //     'provinsi' => fake()->city(),
+        //     'email' => null,
+        //     'pekerjaan' => fake()->jobTitle(),
+        //     'alamat' => fake()->address(),
+        //     'id_siswa' => 1,
+        //     'id_user' => 5,
+        // ]);
 
         // \App\Models\User::factory()->create([           // 11
         //     'nama_lengkap' => 'Arief Widi Pratama',
