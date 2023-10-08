acf7c0e53e0bd8d555ef1bb8d399960ecae86cd2
wahyu12v <ardian.wahyusaputra17@gmail.com>
2023-10-08 14:12:07 +0700

    Tambah Siswa di Guru pada menu management Siswa

--- resources/views/guru/manajemen-siswa/index.blade.php
+++ resources/views/guru/manajemen-siswa/index.blade.php
@@ -47,6 +47,253 @@
     </style>
 
 
+    <div class="card card-custom gutter-b">
+        <div class="card-body">
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#tambahSiswaModal">Tambah Siswa</button>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="tambahSiswaModal" tabindex="-1" role="dialog" aria-labelledby="tambahSiswaModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="tambahSiswaModalLabel">Formulir Tambah siswa Kelas 4 Ibrahim</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-body">
+                        <form action="" method="POST"
+                            id="siswa-form" enctype="multipart/form-data">
+                            
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h5>Data Siswa</h5>
+                                    <div class="form-group">
+                                        <label for="nama">Nama:</label>
+                                        <input type="text" class="form-control" id="nama"
+                                            name="nama" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="nisn">NISN:</label>
+                                        <input type="number" class="form-control" id="nisn"
+                                            name="nisn" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kelas">Kelas:</label>
+                                       <input type="text" class="form-control" id="kelas" name="kelas" disabled placeholder="Kelas 4 Ibrahim">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="jenis-kelamin">Jenis Kelamin:</label>
+                                        <select class="form-control" id="jenis-kelamin" name="jenis_kelamin"
+                                            required>
+                                            <option value="" hidden>pilih</option>
+                                            <option value="Laki-laki">Laki-laki</option>
+                                            <option value="Perempuan">Perempuan</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="agama">Agama:</label>
+                                        <select class="form-control" id="agama" name="agama" required>
+                                            <option value="" hidden>pilih</option>
+                                            <option value="Islam">Islam</option>
+                                            <option value="Kristen">Kristen</option>
+                                            <option value="Katolik">Katolik</option>
+                                            <option value="Hindu">Hindu</option>
+                                            <option value="Budha">Budha</option>
+                                            <option value="Konghucu">Konghucu</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="tempat-lahir">Tempat Lahir:</label>
+                                        <input type="text" class="form-control" id="tempat-lahir"
+                                            name="tempat_lahir" required>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <label for="tanggal-lahir">Tanggal Lahir:</label>
+                                        <input type="date" class="form-control" id="tanggal-lahir"
+                                            name="tanggal_lahir" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kelurahan">Kelurahan:</label>
+                                        <input type="text" class="form-control" id="kelurahan"
+                                            name="kelurahan" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kecamatan">Kecamatan:</label>
+                                        <input type="text" class="form-control" id="kecamatan"
+                                            name="kecamatan" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kabupaten-kota">Kabupaten/Kota:</label>
+                                        <input type="text" class="form-control" id="kabupaten-kota"
+                                            name="kabupaten_kota" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="provinsi">Provinsi:</label>
+                                        <input type="text" class="form-control" id="provinsi"
+                                            name="provinsi" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="alamat">Alamat:</label>
+                                        <textarea class="form-control" id="alamat" name="alamat" required></textarea>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="editFoto">Photo:</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" accept="image/*" onchange="showFileName1(event)" class="custom-file-input"
+                                                    id="editFotoInput1" name="foto">
+                                                <label class="custom-file-label" for="editFotoInput1" id="editFotoLabel1"></label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="editPreviewContainer1" style="display: none;">
+                                        <img id="editPreviewImage1" class="img-fluid" src="#" alt="Preview">
+                                    </div>
+
+                                    <script>
+                                        function showFileName1(event) {
+                                            var input = event.target;
+                                            var fileName = input.files[0].name;
+                                            document.getElementById("editFotoLabel1").textContent = fileName;
+
+                                            var previewContainer = document.getElementById("editPreviewContainer1");
+                                            var previewImage = document.getElementById("editPreviewImage1");
+                                            if (input.files && input.files[0]) {
+                                                var reader = new FileReader();
+
+                                                reader.onload = function (e) {
+                                                    previewImage.src = e.target.result;
+                                                };
+
+                                                previewContainer.style.display = "block";
+                                                reader.readAsDataURL(input.files[0]);
+                                            } else {
+                                                previewContainer.style.display = "none";
+                                            }
+                                        }
+                                    </script>
+
+                                </div>
+
+                                <div class="col-md-6">
+                                    <h5>Data Orang Tua</h5>
+                                    <div class="form-group">
+                                        <label for="nik">NIK:</label>
+                                        <input type="text" class="form-control" id="nik" name="nik" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="nama_ortu">Nama Orang Tua:</label>
+                                        <input type="text" class="form-control" id="nama_ortu" name="nama_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="hubungan">Hubungan:</label>
+                                        <select class="form-control" id="hubungan" name="hubungan" required>
+                                            <option value="" hidden>pilih</option>
+                                            <option value="Orang Tua Kandung">Orang Tua Kandung</option>
+                                            <option value="Orang Tua Angkat">Orang Tua Angkat</option>
+                                            <option value="Saudara">Saudara</option>
+                                            <option value="Keponakan">Keponakan</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="agama">Agama:</label>
+                                        <select class="form-control" id="agama" name="agama_ortu" required>
+                                            <option value="" hidden>pilih</option>
+                                            <option value="Islam">Islam</option>
+                                            <option value="Kristen">Kristen</option>
+                                            <option value="Katolik">Katolik</option>
+                                            <option value="Hindu">Hindu</option>
+                                            <option value="Budha">Budha</option>
+                                            <option value="Konghucu">Konghucu</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="jenis_kelamin_ortu">Jenis Kelamin Orang Tua:</label>
+                                        <select class="form-control" id="jenis_kelamin_ortu" name="jenis_kelamin_ortu" required>
+                                            <option value="" hidden>Pilih</option>
+                                            <option value="Laki-laki">Laki-laki</option>
+                                            <option value="Perempuan">Perempuan</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="no_hp_ortu">Nomor HP Orang Tua:</label>
+                                        <input type="number" class="form-control" id="no_hp_ortu" name="no_hp_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kelurahan_ortu">Kelurahan Orang Tua:</label>
+                                        <input type="text" class="form-control" id="kelurahan_ortu" name="kelurahan_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kecamatan_ortu">Kecamatan Orang Tua:</label>
+                                        <input type="text" class="form-control" id="kecamatan_ortu" name="kecamatan_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="kabupaten_kota_ortu">Kabupaten/Kota Orang Tua:</label>
+                                        <input type="text" class="form-control" id="kabupaten_kota_ortu" name="kabupaten_kota_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="provinsi_ortu">Provinsi Orang Tua:</label>
+                                        <input type="text" class="form-control" id="provinsi_ortu" name="provinsi_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="email_ortu">Email Orang Tua:</label>
+                                        <input type="email" class="form-control" id="email_ortu" name="email_ortu">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="pekerjaan_ortu">Pekerjaan Orang Tua:</label>
+                                        <input type="text" class="form-control" id="pekerjaan_ortu" name="pekerjaan_ortu" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="alamat_ortu">Alamat Orang Tua:</label>
+                                        <textarea class="form-control" id="alamat_ortu" name="alamat_ortu" required></textarea>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Tambah</button>
+                                <button type="button" class="btn btn-success"
+                                    data-dismiss="modal">Batal</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
+
     <div class="">
         <div class="card card-custom gutter-b">
             <div class="card-body">
