756973fb83256e07b6ce228b7111505abe1cbe23
wahyu12v <ardian.wahyusaputra17@gmail.com>
2023-11-23 13:57:09 +0700

    first commit

--- resources/views/guru/tutorial/index.blade.php
+++ resources/views/guru/tutorial/index.blade.php
@@ -1,21 +1,71 @@
 @extends('layouts.main')
 
 @section('main')
-    <div class="">
-        <div class="card card-custom gutter-b">
+    
+    <div class=" ">
+        <div class="card card-custom">
             <div class="card-body">
                 <div class="d-flex flex-column align-items-center">
-                    <h2>
-                        <span style=" text-align: center; text-transform: uppercase;">
-                            Tutorial
-                        </span>
-                    </h2>
+                    <h2 class="text-dark-75 text-center">Tutorial penggunaan Sistem</h2>
+                    <p class="text-dark-50 text-center">Silakan Anda melihat bagaimana cara menggunakan sistem hamka-bs dalam bentuk video.</p>
                 </div>
             </div>
         </div>
     </div>
 
-  
 
+  <div class="card card-custom">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZxSJO2a1mPQ" allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video Cara Tambah Siswa dan orang Tua</h5>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZxSJO2a1mPQ" allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video cara Lihat Akun Siswa dan orang Tua</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZxSJO2a1mPQ" allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video cara Membuka Pertemuan</h5>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZxSJO2a1mPQ" allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video cara Menambahkan Materi Dan Tugas</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+    </div>
+  </div>
 
 @endsection
