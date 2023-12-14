7de4014a28d5ab7f84736ba8409f6f1fe3d43d07
wahyu12v <ardian.wahyusaputra17@gmail.com>
2023-12-15 04:20:11 +0700

    update

--- resources/views/guru/tutorial/index.blade.php
+++ resources/views/guru/tutorial/index.blade.php
@@ -13,70 +13,95 @@
         </div>
     </div>
 
-
-  <div class="card card-custom">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://youtu.be/eOZg7NwfTjo" allowfullscreen></iframe>
+    <div class="card card-custom">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eOZg7NwfTjo" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Tambah Siswa Dan Wali Murid dalam sistem HamkaBS</h5>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Tutorial Tambah Siswa Dan Wali Murid dalam sistem HamkaBS</h5>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/woCHzIFvIt0" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Melakukan Absensi dan Memberikan Nilai Siswa dalam Sistem HamkaBS</h5>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://youtu.be/woCHzIFvIt0" allowfullscreen></iframe>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Tutorial Melakukan Absensi dan Memberikan Nilai Siswa dalam Sistem HamkaBS</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row mt-5">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://youtu.be/Wnke70pym64" allowfullscreen></iframe>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Tutotrial Melihat dan Mengedit Akun Siswa Dan Wali Murid dalam Sistem HamkaBS</h5>
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Wnke70pym64" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Melihat dan Mengedit Akun Siswa Dan Wali Murid dalam Sistem HamkaBS</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://youtu.be/7XsOI33XcvU" allowfullscreen></iframe>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Tutorial Tambah Latihan Dan Materi di Sistem HamkaBS</h5>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/7XsOI33XcvU" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Tambah Latihan Dan Materi di Sistem HamkaBS</h5>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://youtu.be/jZOcGCaTdBg" allowfullscreen></iframe>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/jZOcGCaTdBg" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Tambah Mata Pelajaran dalam Sistem HamkaBS</h5>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Tutorial Tambah Mata Pelajaran dalam Sistem HamkaBS</h5>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/xv7VZVMBD0c" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Mengatur Nilai KKM dalam Sistem HamkaBS</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nJO3yCQ8H-k" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Tutorial Rekap Nilai dalam Sistem HamkaBS</h5>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    </div>
-  </div>
+
 
 @endsection
